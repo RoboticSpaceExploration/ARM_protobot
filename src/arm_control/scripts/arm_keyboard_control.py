1f5a36b630518ff4584aac6d704f30a3d3e06079
Joshua Galeria <jgaleria@hawaii.edu>
2022-04-19 22:44:06 -1000

    Initial keyboard teleop

--- src/arm_control/scripts/arm_keyboard_control.py
+++ src/arm_control/scripts/arm_keyboard_control.py
@@ -1,2 +1,22 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
+# license removed for brevity
+import rospy
+from std_msgs.msg import Float64
+
+def talker():
+    pub = rospy.Publisher('/arm/joint5_position_controller/command', Float64, queue_size=10)
+    rospy.init_node('arm_talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        #hello_str = "hello world %s" % rospy.get_time()
+        position = 2.0
+        rospy.loginfo(position)
+        pub.publish(position)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
 
